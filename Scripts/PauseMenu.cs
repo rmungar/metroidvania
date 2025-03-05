accb23dfe556735b992d9ba33632c9be30899f6d
rmungar <mgraul@iesrafaelalberti.es>
2025-03-05 13:23:48 +0100

    Solucionado el problema con el pauseMenu

--- Scripts/PauseMenu.cs
+++ Scripts/PauseMenu.cs
@@ -7,9 +7,20 @@ public partial class PauseMenu : Control
 	bool isPaused = false;
 
 
+	
+
 	public override void _Ready(){
 		GetTree().Paused = false;
 		Visible = false;
+	}
+
+
+	public override void _Process(double delta)
+	{	
+		CanvasLayer cl = GetParent<CanvasLayer>();
+		Camera2D cam = cl.GetParent<Camera2D>();
+		PlayerController pc = cam.GetParent<PlayerController>();
+		GlobalPosition = pc.GlobalPosition + new Vector2(-192,-178);
 	}
 
 
